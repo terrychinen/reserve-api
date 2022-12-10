2aaa20ffc9a88519797aa86a5066418a7ef1c655
terrychinen <terrychinen55@gmail.com>
2022-12-10 06:41:00 -0500

    Add: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfigService,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/database-config.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  constructor(private _configService: ConfigService) {}
+
+  private _getValue(key: string): string | any {
+    const value = this._configService.get(key);
+    if (!value) throw new Error(`Config error: missing env.${key}`);
+    return value;
+  }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: this._getValue('DATABASE_TYPE'),
+      host: this._getValue('DATABASE_HOST'),
+      port: +this._getValue('DATABASE_PORT'),
+      username: this._getValue('DATABASE_USER'),
+      password: this._getValue('DATABASE_PASSWORD'),
+      database: this._getValue('DATABASE_NAME'),
+      autoLoadEntities: this._getValue('DATABASE_AUTOLOADENTITIES'),
+      synchronize: this._getValue('DATABASE_SYNCHRONIZE'),
+    };
+  }
+}
