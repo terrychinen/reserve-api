1c24708feda8c5bf115a915f1f1d3470c4d4a3f7
terrychinen <terrychinen55@gmail.com>
2022-12-11 16:02:02 -0500

    Add: configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Reserve Api')
+    .setDescription('This is a reserve api example')
+    .setVersion('1.0')
+    .addTag('reserve')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.NEST_PORT);
 }
